71b2a916a4ac35c7169a71f468241c146054401f
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2021-09-20 21:10:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,73 @@
 class Game:
     def __init__(self):
         # initialize game window, etc
-        pg.init()
-        pg.mixer.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption(TITLE)
-        self.clock = pg.time.Clock()
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
         self.running = True
 
     def new(self):
         # start a new game
-        self.all_sprites = pg.sprite.Group()
-        self.player = Player()
-        self.all_sprites.add(self.player)
-        self.run()
+        all_sprites = pygame.sprite.Group()
+        pokemongroup = pygame.sprite.Group()
+        bushesgroup = pygame.sprite.Group()
+        wildpokemongroup = pygame.sprite.Group()
+
+        Bush = BushesAndStuff()
+        # all_sprites.add(Bush)
+        bushesgroup.add(Bush)
+        Bush2 = BushesAndStuff()
+        # all_sprites.add(Bush2)
+        bushesgroup.add(Bush2)
+        Bush3 = BushesAndStuff()
+        # all_sprites.add(Bush3)
+        bushesgroup.add(Bush3)
+        Bush4 = BushesAndStuff()
+        # all_sprites.add(Bush4)
+        bushesgroup.add(Bush4)
+        Bush5 = BushesAndStuff()
+        # all_sprites.add(Bush5)
+        bushesgroup.add(Bush5)
+        Bush6 = BushesAndStuff()
+        # all_sprites.add(Bush6)
+        bushesgroup.add(Bush6)
+        Bush7 = BushesAndStuff()
+        # all_sprites.add(Bush7)
+        bushesgroup.add(Bush7)
+        Bush8 = BushesAndStuff()
+        # all_sprites.add(Bush8)
+        bushesgroup.add(Bush8)
+        Bush9 = BushesAndStuff()
+        # all_sprites.add(Bush9)
+        bushesgroup.add(Bush9)
+        Bush10 = BushesAndStuff()
+        # all_sprites.add(Bush10)
+        bushesgroup.add(Bush10)
+        Bush11 = BushesAndStuff()
+        # all_sprites.add(Bush11)
+        bushesgroup.add(Bush11)
+        Bush12 = BushesAndStuff()
+        # all_sprites.add(Bush12)
+        bushesgroup.add(Bush12)
+        Bush13 = BushesAndStuff()
+        # all_sprites.add(Bush13)
+        bushesgroup.add(Bush13)
+        Bush14 = BushesAndStuff()
+        # all_sprites.add(Bush14)
+        bushesgroup.add(Bush14)
+        Bush15 = BushesAndStuff()
+        # all_sprites.add(Bush15)
+        bushesgroup.add(Bush15)
+
+        MyCharac = Guy()
+        all_sprites.add(MyCharac)
+
+        Pikachu = PichuEvo(5)
+        pokemongroup.add(Pikachu)
+        Wild_Pichu = RoamPichu(5)
+        wildpokemongroup.add(Wild_Pichu)
 
     def run(self):
         # Game Loop
@@ -30,16 +84,16 @@ class Game:
 
     def events(self):
         # Game Loop - events
-        for event in pg.event.get():
+        for event in pygame.event.get():
             # check for closing window
-            if event.type == pg.QUIT:
+            if event.type == pygame.QUIT:
                 if self.playing:
                     self.playing = False
                 self.running = False
 
     def draw(self):
         # Game Loop - draw
-        self.screen.fill(BLACK)
+        self.screen.fill(var.BLACK)
         self.all_sprites.draw(self.screen)
         # *after* drawing everything, flip the display
         pg.display.flip()
@@ -58,4 +112,4 @@ while g.running:
     g.new()
     g.show_go_screen()
 
-pg.quit()
+pygame.quit()
